1e9a589ce4177c16339e77bfc19840233447fb6d
Emmet Haggard <143468532+ehaggardgav@users.noreply.github.com>
2024-07-03 16:49:23 +0000

    update formatting for roster name additions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,16 +8,18 @@ import java.util.ArrayList;
 public class Main {
   public static void main(String[] args) {
   ArrayList<String> roster = new ArrayList<>();
-    roster.add(Amy);
-    roster.add(Bill);
-    roster.add(Colin);
-    roster.add(Danny);
-    roster.add(Emma);
-    roster.add(Figus);
-    roster.add(George);
-    roster.add(Hannah);
-    roster.add(Imogene);
-    roster.add(Kyle);
+    roster.add("Amy");
+    roster.add("Bill");
+    roster.add("Colin");
+    roster.add("Danny");
+    roster.add("Emma");
+    roster.add("Figus");
+    roster.add("George");
+    roster.add("Hannah");
+    roster.add("Imogene");
+    roster.add("Kyle");
+
+  
     
   }
 

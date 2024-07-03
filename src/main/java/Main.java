68686083892b78b960a23912627f86e0bd93f21a
Emmet Haggard <143468532+ehaggardgav@users.noreply.github.com>
2024-07-03 16:21:58 +0000

    import Scanner and ArrayList

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,12 @@
 
 // import org.junit.jupiter.api.Test;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    
   }
 
   // @Test
